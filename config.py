6a8872e3f274cf3d8de852de25ba92c0cecf8594
aryforker <97401553+aryforker@users.noreply.github.com>
2025-11-04 13:45:32 -0800

    Update config.py
    Signed-off-by: aryforker <97401553+aryforker@users.noreply.github.com>

--- config.py
+++ config.py
@@ -5,21 +5,21 @@ load_dotenv()
 
 class Config:
     def __init__(self):
-        self.API_ID = int(getenv("API_ID", 0))
-        self.API_HASH = getenv("API_HASH")
+        self.API_ID = int(getenv("API_ID", "8154886"))
+        self.API_HASH = getenv("API_HASH", "d2976cd3599122c571f8e5afd54c6d04")
 
-        self.BOT_TOKEN = getenv("BOT_TOKEN")
-        self.MONGO_URL = getenv("MONGO_URL")
+        self.BOT_TOKEN = getenv("BOT_TOKEN", "8039894426:AAFrF0vmmpiqW8YKp99Ix96b52CYopucaqg")
+        self.MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Aryana:1234567890@cluster0.eblez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-        self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
-        self.OWNER_ID = int(getenv("OWNER_ID", 0))
+        self.LOGGER_ID = int(getenv("LOGGER_ID", "-1003261133706"))
+        self.OWNER_ID = int(getenv("OWNER_ID", "970535949"))
 
-        self.SESSION1 = getenv("SESSION", None)
+        self.SESSION1 = getenv("SESSION", "BAB8bwYAP6bT5exAX2beZ7MXsWB-4crm7Vrl8hE7SWDHQb75o2QqeDDDmwb3NwunyksjEx7-GltybYHEvuEhz0uty5-cdk-9hrcEiu9WXCr6L0JVAmCuUJnqD89Cy0cFCyGyyVDmfy96m8xi5Uc7iEIGN3NYY9_YaVcjreCwCgzgpqQHvHoFtCBX2YQfMc_WHTS5EvtPgtDivfFsiDglWuQyXTzqZy9IWCClhtK1VgCw5B9EFC5JOTqChJIQ8jI0Wn6p07CzCDJEIifMaIJT17kNE8Dj0WnP6Kjvsxz3PjV8Ef1J4qkDLlqoXfMTM_5szyySHhGrHgt72akUmX5s0wCBhYy7aQAAAAGzkOFJAA")
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/The_Aryana_PY")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/The_Aryana_PY")
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
